1db63e1f89b122a647e15ddf7921efed57145793
Ankur Gupta <103580256+ankurgupta9@users.noreply.github.com>
2025-02-10 22:26:24 +0530

    Add files via upload

---
+++ src/main/java/com/MetroEase/MetroEaseApplication.java
@@ -0,0 +1,13 @@
+package com.MetroEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroEaseApplication.class, args);
+	}
+
+}
